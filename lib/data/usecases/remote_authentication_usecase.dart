51a1329f3bf1b59d0d81e950c97f99cdc3b4c30a
Eduardo Cercal de Souza Aracema <eduardo.cercal@hotmail.com>
2023-03-07 17:43:30 -0300

    make remote authentication implements authentication

--- lib/data/usecases/remote_authentication_usecase.dart
+++ lib/data/usecases/remote_authentication_usecase.dart
@@ -1,13 +1,14 @@
 import 'package:clean_flutter_login_app/data/http/http.error.dart';
 import 'package:clean_flutter_login_app/data/models/authentication_params_model.dart';
 import 'package:clean_flutter_login_app/domain/entities/account_entity.dart';
+import 'package:clean_flutter_login_app/domain/usecases/authentication_usecase.dart';
 import 'package:clean_flutter_login_app/utils/domain_error.dart';
 
 import '../../domain/entities/authentication_params_entity.dart';
 import '../http/http_client.dart';
 import '../models/account_model.dart';
 
-class RemoteAuthentication {
+class RemoteAuthentication implements AuthenticationUseCase {
   final HttpClient httpClient;
   final String url;
 
@@ -16,6 +17,7 @@ class RemoteAuthentication {
     required this.url,
   });
 
+  @override
   Future<AccountEntity> auth(AuthenticationParamsEntity params) async {
     final AuthenticationParamsModel model =
         AuthenticationParamsModel.fromEntity(params);
