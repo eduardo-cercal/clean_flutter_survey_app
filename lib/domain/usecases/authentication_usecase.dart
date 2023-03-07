cd125b6b7840a8156c0212caafcd7692555bcfcb
Eduardo Cercal de Souza Aracema <eduardo.cercal@hotmail.com>
2023-03-07 13:57:23 -0300

    change authentication use case to receive a model

--- lib/domain/usecases/authentication_usecase.dart
+++ lib/domain/usecases/authentication_usecase.dart
@@ -1,8 +1,15 @@
 import 'package:clean_flutter_login_app/domain/entities/account_entity.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
-    required String email,
-    required String password,
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({
+    required this.email,
+    required this.password,
   });
 }
